159a775fb324fa54b17b9e903423d70e294130f4
AKHIL E A <akhil.ea@factweavers.com>
2021-10-26 23:55:50 +0530

    added created name

--- recipe_house_backend/apps/post/api/v1/serializers.py
+++ recipe_house_backend/apps/post/api/v1/serializers.py
@@ -7,30 +7,42 @@ class TagSerializer(serializers.ModelSerializer):
     """
     Serializer for Tag model
     """
+    created_by_name = serializers.SerializerMethodField()
 
     class Meta(object):
         model = Tag
         fields = '__all__'
+
+    def get_created_by_name(self, obj):
+        return obj.created_by.email
 
 
 class CategorySerializer(serializers.ModelSerializer):
     """
     Serializer for Category model
     """
+    created_by_name = serializers.SerializerMethodField()
 
     class Meta(object):
         model = Category
         fields = '__all__'
+
+    def get_created_by_name(self, obj):
+        return obj.created_by.email
 
 
 class CuisineSerializer(serializers.ModelSerializer):
     """
     Serializer for Cuisine model
     """
+    created_by_name = serializers.SerializerMethodField()
 
     class Meta(object):
         model = Cuisine
         fields = '__all__'
+
+    def get_created_by_name(self, obj):
+        return obj.created_by.email
 
 
 class PostSerializer(serializers.ModelSerializer):
